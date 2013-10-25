11ef68c2960a8fdb5049d20a26b5df89ec26774c
Jeremy McNevin <jeremy@fastpencil.com>
2013-10-25 08:41:45 -0500

    Improved efficiency.

--- lib/i18n/backend/active_record/translation.rb
+++ lib/i18n/backend/active_record/translation.rb
@@ -73,7 +73,7 @@ module I18n
           end
 
           def available_locales
-            Translation.distinct(:locale).map { |t| t.locale.to_sym }
+            Translation.distinct(:locale).pluck(:locale).collect(&:to_sym)
           end
         end
 
